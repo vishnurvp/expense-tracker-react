663548895e6bcc5ed8b5c480d90be72473616af9
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-07-15 23:13:34 +0530

    Task_1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import SignUp from './components/signup/SignUp';
 
 function App() {
   return (
     <div className="App">
       <h1>Expense Tracker App</h1>
+      <SignUp/>
     </div>
   );
 }
---
+++ src/components/signup/SignUp.js
@@ -0,0 +1,67 @@
+import React, { Fragment } from "react";
+import classes from "./SignUp.module.css";
+
+const SignUp = () => {
+  const signuphandler = async (event) => {
+    event.preventDefault();
+    const email = event.target.elements["email"].value;
+    const password1 = event.target.elements["password1"].value;
+    const password2 = event.target.elements["password2"].value;
+    if (password1 === "" || password1.length < 8 || password1 !== password2) {
+      alert(
+        `Password cannot be empty \nPassword should be atleast 8 charecters long \nBoth Passwords should match`
+      );
+    } else {
+      try {
+        const response = await fetch(
+          `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCDH1TbzmhnXSFIsJYaiixXeP03MX4rw0Q`,
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email: email,
+              password: password1,
+              returnSecureToken: true,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await response.json();
+        //   console.log(data);
+        alert(`Signup Successfull \nYour Email: ${data.email}`);
+      } catch (err) {
+        alert(err.error.message);
+      }
+      //   console.log(email, password1, password2);
+      event.target.elements["email"].value = "";
+      event.target.elements["password1"].value = "";
+      event.target.elements["password2"].value = "";
+    }
+  };
+
+  return (
+    <Fragment>
+      <form className={classes.form} onSubmit={signuphandler}>
+        <label htmlFor="email">Email</label>
+        <br />
+        <input id="email" type="email"></input>
+        <br />
+        <label htmlFor="password1">Password</label>
+        <br />
+        <input id="password1" type="password"></input>
+        <br />
+        <label htmlFor="password2">Confirm Password</label>
+        <br />
+        <input id="password2" type="password"></input>
+        <br />
+        <button id="signUpBtn" type="submit">
+          Sign Up
+        </button>
+        <br />
+      </form>
+    </Fragment>
+  );
+};
+
+export default SignUp;
