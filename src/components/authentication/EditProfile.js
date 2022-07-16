23906995d54208d49315a35423caa7335e34b6e7
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-07-16 12:57:19 +0530

    Task_4

--- src/components/authentication/EditProfile.js
+++ src/components/authentication/EditProfile.js
@@ -1,26 +1,52 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useEffect, useRef } from "react";
 import AuthContext from "../../context/auth-context";
 
 const EditProfile = (props) => {
-    const authCtx = useContext(AuthContext);
+  const authCtx = useContext(AuthContext);
+  const APIkey = authCtx.APIkey;
+  const dispName = useRef();
+  const dispImg = useRef();
 
-  const profileEditSubmitHandler = async (event) => {
-    event.preventDefault();
-    const name = event.target.elements["nameInp"].value;
-    const photoUrl = event.target.elements["photoUrlInp"].value;
-    console.log(name, photoUrl);
-    const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:update?key=${authCtx.APIkey}`, {
+  useEffect(()=>{
+    fetch(`https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${APIkey}`,
+    {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
             idToken: authCtx.token,
-            displayName : name,
-            photoUrl: photoUrl,
-            returnSecureToken:true,
         }),
-    })
+    }
+    )
+    .then(response=> response.json()
+    .then(data=>{
+        console.log(data);
+        dispName.current.value = data.users[0].displayName || '';
+        dispImg.current.value = data.users[0].photoUrl || '';
+    }));
+  },[APIkey, authCtx.token]);
+
+  const profileEditSubmitHandler = async (event) => {
+    event.preventDefault();
+    const name = event.target.elements["nameInp"].value;
+    const photoUrl = event.target.elements["photoUrlInp"].value;
+    console.log(name, photoUrl);
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${authCtx.APIkey}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          displayName: name,
+          photoUrl: photoUrl,
+          returnSecureToken: true,
+        }),
+      }
+    );
     const data = await response.json();
     console.log(data);
     event.target.elements["nameInp"].value = "";
@@ -32,13 +58,13 @@ const EditProfile = (props) => {
         <h3>Edit Profile</h3>
         <label htmlFor="nameInp">Your Name</label>
         <br />
-        <input id="nameInp"></input>
+        <input ref={dispName} id="nameInp"></input>
         <br />
         <label htmlFor="photoUrlInp">Your Photo URL</label>
         <br />
-        <input id="photoUrlInp"></input>
+        <input ref={dispImg} id="photoUrlInp"></input>
         <br />
-        <button id="submitEditBtn">Submit</button>
+        <button id="submitEditBtn">Edit</button>
       </form>
     </Fragment>
   );
