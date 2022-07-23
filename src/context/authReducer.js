07f77ae3e05257d87d907fde8d97e010acc37a3d
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-07-23 14:41:34 +0530

    remember_login

--- src/context/authReducer.js
+++ src/context/authReducer.js
@@ -2,11 +2,11 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialAuthState = {
   apiKey: "AIzaSyCDH1TbzmhnXSFIsJYaiixXeP03MX4rw0Q",
-  isAuthenticated: false,
-  idToken: '',
-  email: '',
-  cleanEmail: '',
-  isEmailVerified: false,
+  isAuthenticated: localStorage.getItem("token") ? true : false,
+  idToken: localStorage.getItem("token") || "",
+  email: localStorage.getItem("email") || "",
+  cleanEmail: localStorage.getItem("cleanEmail") || "",
+  isEmailVerified: localStorage.getItem("emailVerified") || false,
 };
 
 const authSlice = createSlice({
@@ -16,18 +16,23 @@ const authSlice = createSlice({
     login(state, action) {
       state.isAuthenticated = true;
       state.idToken = action.payload;
+      localStorage.setItem("token", action.payload);
     },
     logout(state) {
       state.isAuthenticated = false;
+      localStorage.clear();
     },
     setEmail(state, action) {
       state.email = action.payload;
+      localStorage.setItem("email", action.payload);
     },
     setCleanEmail(state, action) {
       state.cleanEmail = action.payload;
+      localStorage.setItem("cleanEmail", action.payload);
     },
     setIsEmailVerified(state, action) {
       state.isEmailVerified = action.payload;
+      localStorage.setItem("emailVerified", action.payload);
     },
   },
 });
